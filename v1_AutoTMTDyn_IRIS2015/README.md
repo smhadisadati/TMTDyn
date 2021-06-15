22f5d45aabc330e0c2cb1df79ad91c0521482169
S.M.Hadi Sadati <m.hadi.sadati@gmail.com>
2021-06-15 12:23:51 +0100

    Delete all!

--- v1_AutoTMTDyn_IRIS2015/README.md
+++
@@ -1,4 +0,0 @@
-AutoTMTDyn is a Matlab based package, for automatic derivation of TMT Equation of Motion (EOM) for rigid body dynamic systems, and nonlinear controller design.
-
-Please cite our relevant papers if you decided to use this package or methods presented in it:
-- [S.M.H. Sadati, S. Naghibi, and M. Naraghi, “An Automatic Algorithm to Derive Linear Vector Form of Lagrangian Equation of Motion with Collision and Constraint,” Procedia Computer Science, vol. 76, pp. 217–222, 2015.](https://www.researchgate.net/publication/290003554_An_Automatic_Algorithm_to_Derive_Linear_Vector_Form_of_Lagrangian_Equation_of_Motion_with_Collision_and_Constraint)
--- v6_Latest_Version_Examples/eom - exp2_EBR (3n_l)/EOM_input.m
+++
@@ -1,8 +0,0 @@
-function [tspan, nz, z0, par_mex] = EOM_input()
-tspan = [ 7         0.01         49.2 ] ;
-nz = 36 ;
-z0 = [ 1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06       1e-06           0           0           0           0           0           0           0           0           0           0           0           0           0           0           0           0           0           0 ]' ;
-temp = coder.load( 'user_pars.mat' );
-par_mex.user_pars = temp.user_pars ;
-par_mex.var = [ 1820           205000           0.0045           0.0125             0.05              0.1              0.1              0.1            0.001            0.001            0.001                2                0                0             9.81          0.01347           0.0145           0.0075           0.0025           0.0085      1.504117308                0                0                0                0                0                0                0                0                0                0                0                0                1 ] ;
-par_mex.t_equil_i = [ 7 ] ;
